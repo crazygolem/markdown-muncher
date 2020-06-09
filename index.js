43dd9b69b9d062cb45a8c4cfb8a1ad86d2ac5772
Junior <superjun1@gmail.com>
2020-06-09 08:01:32 +0200

    Sanitize output HTML

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ var unified = require('unified')
 var markdown = require('remark-parse')
 var remark2rehype = require('remark-rehype')
 var html = require('rehype-stringify')
+var sanitize = require('rehype-sanitize')
 
 var processor = unified()
   .use(markdown, {commonmark: true})
   .use(remark2rehype)
   .use(html)
+  .use(sanitize)
 
 module.exports = function process(md) {
     return new Promise((res, rej) => processor.process(md, function(err, file) {
